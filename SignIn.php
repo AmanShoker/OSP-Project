06d178d6d91cd6c7f458f2472feb261bfdb6d90b
AmanShoker <amanshoker04@gmail.com>
2025-02-22 15:35:07 -0500

    completed sign in with web remembering

--- SignIn.php
+++ SignIn.php
@@ -9,11 +9,17 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 if ($UTC->validLogin($conn,$username,$password)) {
+
+    setcookie('isSignedIn', 'true', time() + (86400), "/"); 
+    setcookie('username', $username, time() + (86400), "/");
+
     header("Location: Homepage.html");
     exit();
 } else {
     header("Location: SignIn.html?error=insertfailed");
     exit();
 }
+
+$conn->close();
 
 ?>
